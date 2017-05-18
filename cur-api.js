0adccb62083f31badaaedcafd92ec3a2409c4bdf
Sasiliyu Adetunji <shazily012@gmail.com>
2017-05-17 19:48:08 -0700

    updated version of the api app

--- cur-api.js
+++ cur-api.js
@@ -7,7 +7,7 @@ let chalk = require('chalk');
 
 
 console.log(chalk.blue('Currency')); 
-console.log(chalk.green('This app converts your Euros to any other Currency')); 
+console.log(chalk.green('This app converts your Euros to USD ')); 
 
  
 //Function to convert from eur 
@@ -16,7 +16,7 @@ console.log(chalk.green('This app converts your Euros to any other Currency'));
  properties: { 
     to: { 
 
-      message: '3 letter currency Alphabet you are converting TO: USD, GBP' 
+      message: chalk.red('Enter the 3 alphabet of the currency you are converting of USD i.e USD')
 
     }, 
       } 
@@ -24,16 +24,16 @@ console.log(chalk.green('This app converts your Euros to any other Currency'));
 prompt.start(); 
 prompt.get(cur, function (err, result) { 
     let from    = result.from, 
-        to  = result.to; 
+        to  = result.to.toUpperCase(); 
     let link = "http://api.fixer.io/latest?symbols=USD" + to ; 
     request(link, function (err, res, data){ 
     if (err) return console.log(err); 
 
     data = JSON.parse(data); 
-//Display currency information 
+    //Display currency information 
     console.log('The conversion of 1 ' + 'Euros' + ' TO ' + ' ' + '\n' +
 
-      to + 'as at : ' + data.date + ' is ' +  data.rates) ; 
+      to + ' as at : ' + data.date + ' is ' +  data.rates.USD) ; 
     } 
   ) 
 }) 
